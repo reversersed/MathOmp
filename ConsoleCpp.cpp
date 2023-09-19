d4aa67552e274ada6033a9062407f216bbfe9db4
hedgehog <myregisg@gmail.com>
2023-09-19 22:13:33 +0300

    Completed adding methods

--- ConsoleCpp.cpp
+++ ConsoleCpp.cpp
@@ -17,14 +17,15 @@ double f(double* y, double time, int i)
 	{
 	case 0: return y[1];
 	case 1: return (-0.19 * time * y[1] - pow(time,2.0) * y[0]);
+	default: return 0.0;
 	}
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	double time_begin, time_end, time_elapsed;
-
+	double time_begin, time_end;
+	
 	//Euler
 	std::cout << "Явный метод Эйлера:" << std::endl;
 	time_begin = omp_get_wtime();
@@ -129,6 +130,33 @@ int main()
 	for (int i = 0; i < n; i++)
 		std::cout << "y[" << i + 1 << "] = " << y[i] << "\n";
 
+	//Euler
+	std::cout << std::endl << "Неявный метод Эйлера:" << std::endl;
+
+	y[0] = 0.0, y[1] = 0.05;
+	yy[0] = 0, yy[1] = 0;
+	double a[2][2] = { {-1 / 0.001,1},{0,-1 / 0.001} },
+			fs[n] = { 0.0 };
+
+	time_begin = omp_get_wtime();
+	for (double x = t0; x <= t_max; x += tau)
+	{
+		double d[3] = {};
+		for (int i = 0; i < n; i++)
+			fs[i] = -f(y, x, i);
+
+		d[0] = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+		d[1] = a[1][1] * fs[0] - fs[1] * a[0][1];
+		d[2] = a[0][0] * fs[1] - fs[0] * a[1][0];
+		for (int i = 0; i < n; i++)
+			yy[i] = d[i + 1] / d[0];
+		for (int i = 0; i < n; i++)
+			y[i] = y[i] + yy[i];
+	}
+	time_end = omp_get_wtime();
+	std::cout << "Время расчетов: " << (time_end - time_begin) << " секунд" << std::endl;
+
+	for (int i = 0; i < n; i++)
+		std::cout << "y[" << i + 1 << "] = " << y[i] << "\n";
 	return 0;
 }
-
